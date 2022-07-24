e1ba31e3604b459efa14a3619c5b06302ffc07d1
nikochetov <nikochetov@gmail.com>
2022-07-24 21:26:34 +0300

    update form validation

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { Formik } from 'formik';
+import { Button } from 'react-bootstrap';
+import authFormSchema from '../validators/auth-form-validator';
 
 const Login = () => {
+  // const [validated, setValidated] = useState(false);
+
   const submit = (values) => {
     console.log('submit values::::::', values);
   };
 
   return (
-    <Formik initialValues={{ login: '', password: '' }} onSubmit={(values) => submit(values)}>
-      {({ errors, touched }) => (
-        <Form>
+    <Formik validationSchema={authFormSchema} initialValues={{ login: '', password: '' }} onSubmit={(values) => submit(values)}>
+      {({
+        handleSubmit, handleChange, values, errors, touched,
+      }) => (
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <FloatingLabel
               controlId="floatingInput"
@@ -23,9 +29,14 @@ const Login = () => {
                 required
                 type="text"
                 placeholder="Enter login"
+                value={values.login}
+                onChange={handleChange}
                 isValid={touched.login && !errors.login}
               ></Form.Control>
             </FloatingLabel>
+            <Form.Control.Feedback type="invalid">
+              {errors.login}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <FloatingLabel
@@ -37,11 +48,17 @@ const Login = () => {
                 name="password"
                 required
                 type="text"
+                value={values.password}
+                onChange={handleChange}
                 placeholder="Enter password"
                 isValid={touched.password && !errors.password}
               ></Form.Control>
             </FloatingLabel>
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
           </Form.Group>
+          <Button type="submit">Submit</Button>
         </Form>
       )}
     </Formik>
--- frontend/src/validators/auth-form-validator.js
+++ frontend/src/validators/auth-form-validator.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
-const authFormValidator = () => yup.object().shape({
-  login: yup.string().min(3).max(50).required(),
-  password: yup.string().min(3).max(50).required(),
+const authFormSchema = () => yup.object().shape({
+  login: yup.string().min(3, 'Too short').max(50, 'Too long').required('Required'),
+  password: yup.string().min(3, 'Too short').max(50, 'Too long').required('Required'),
 });
 
-export default authFormValidator;
+export default authFormSchema;
