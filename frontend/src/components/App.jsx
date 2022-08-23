bdd204de455d9c2862270be91901cb01f1430d78
nikochetov <nikochetov@gmail.com>
2022-08-24 00:00:00 +0300

    added AuthProvider in App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,24 @@
 import '../styles/App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './Login.jsx';
 import NotMatch from './NotMatch.jsx';
+import AuthContext from '../../contexts';
+
+const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    setLoggedIn(false);
+    localStorage.removeItem('userId');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 const App = () => (
     <Router>
