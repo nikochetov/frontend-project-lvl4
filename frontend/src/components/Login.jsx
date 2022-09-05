1028ed7cd56b8acf8564abcf441ec627e1859c63
nikochetov <nikochetov@gmail.com>
2022-09-05 22:57:52 +0300

    update styles

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { Formik } from 'formik';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
 import authFormSchema from '../validators/auth-form-validator';
+import useAuth from '../hooks';
+import routes from '../routes';
 
 const Login = () => {
-  // const [validated, setValidated] = useState(false);
+  const auth = useAuth();
+  const [error, setError] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { state } = location;
 
   const submit = (values) => {
-    console.log('submit values::::::', values);
+    const login = async () => {
+      try {
+        const response = await axios.post(routes.loginPath(), values);
+        navigate(state?.from || '/');
+        auth.logIn();
+        const { token } = response.data;
+        localStorage.setItem('userId', JSON.stringify({ token }));
+      } catch (err) {
+        setError(err);
+      }
+    };
+
+    login();
   };
 
   return (
@@ -17,7 +37,10 @@ const Login = () => {
       {({
         handleSubmit, handleChange, values, errors, touched,
       }) => (
-        <Form onSubmit={handleSubmit}>
+        <Card className="mt-5 col-md-6 mx-auto">
+          <Card.Header>Авторизация</Card.Header>
+          <Card.Body>
+         <Form onSubmit={handleSubmit}>
           <Form.Group>
             <FloatingLabel
               controlId="floatingInput"
@@ -62,6 +85,8 @@ const Login = () => {
           </Form.Group>
           <Button type="submit">Submit</Button>
         </Form>
+          </Card.Body>
+        </Card>
       )}
     </Formik>
   );
