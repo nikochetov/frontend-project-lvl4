7590fd60c7ca78650fcd05248ff2a565679a2265
nikochetov <nikochetov@gmail.com>
2022-09-07 16:52:48 +0300

    fix login request

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,12 +11,13 @@ import routes from '../routes';
 
 const Login = () => {
   const auth = useAuth();
-  const [error, setError] = useState(null);
+  const [authFailed, setAuthFailed] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   const { state } = location;
 
   const submit = (values) => {
+    setAuthFailed(false);
     const login = async () => {
       try {
         const response = await axios.post(routes.loginPath(), values);
@@ -25,7 +26,7 @@ const Login = () => {
         const { token } = response.data;
         localStorage.setItem('userId', JSON.stringify({ token }));
       } catch (err) {
-        setError(err);
+        setAuthFailed(true);
       }
     };
 
@@ -33,58 +34,66 @@ const Login = () => {
   };
 
   return (
-    <Formik validationSchema={authFormSchema} initialValues={{ login: '', password: '' }} onSubmit={(values) => submit(values)}>
+    <Formik
+      validationSchema={authFormSchema}
+      initialValues={{ username: '', password: '' }}
+      onSubmit={(values) => submit(values)}
+    >
       {({
-        handleSubmit, handleChange, values, errors, touched,
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+        touched,
       }) => (
-        <Card className="mt-5 col-md-6 mx-auto">
+        <Card className="mt-5 col-md-7 col-sm-10 mx-auto">
           <Card.Header>Авторизация</Card.Header>
           <Card.Body>
-         <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Login"
-              className="mb-3"
-            >
-              <Form.Control
-                name="login"
-                required
-                type="text"
-                placeholder="Enter login"
-                value={values.login}
-                onChange={handleChange}
-                isInvalid={!!errors.login}
-                isValid={touched.login && !errors.login}
-              ></Form.Control>
-            </FloatingLabel>
-            <Form.Control.Feedback type="invalid">
-              {errors.login}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Password"
-              className="mb-3"
-            >
-              <Form.Control
-                name="password"
-                required
-                type="text"
-                value={values.password}
-                onChange={handleChange}
-                placeholder="Enter password"
-                isInvalid={!!errors.password}
-                isValid={touched.password && !errors.password}
-              ></Form.Control>
-            </FloatingLabel>
-            <Form.Control.Feedback type="invalid">
-              {errors.password}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button type="submit">Submit</Button>
-        </Form>
+           <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Username"
+                className="mb-3"
+              >
+                <Form.Control
+                  name="username"
+                  required
+                  type="text"
+                  placeholder="Enter username"
+                  value={values.username}
+                  onChange={handleChange}
+                  isInvalid={authFailed}
+                  isValid={touched.username && !authFailed}
+                ></Form.Control>
+              </FloatingLabel>
+              <Form.Control.Feedback type="invalid">
+                {errors.username}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  name="password"
+                  required
+                  type="text"
+                  value={values.password}
+                  onChange={handleChange}
+                  placeholder="Enter password"
+                  isInvalid={authFailed}
+                  isValid={touched.password && !authFailed}
+                ></Form.Control>
+              </FloatingLabel>
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </Form>
           </Card.Body>
         </Card>
       )}
--- frontend/src/validators/auth-form-validator.js
+++ frontend/src/validators/auth-form-validator.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 const authFormSchema = () => yup.object().shape({
-  login: yup.string().min(3, 'Too short').max(50, 'Too long').required('Required'),
+  username: yup.string().min(3, 'Too short').max(50, 'Too long').required('Required'),
   password: yup.string().min(3, 'Too short').max(50, 'Too long').required('Required'),
 });
 
