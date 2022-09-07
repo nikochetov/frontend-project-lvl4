5598d6ea0a49c27caf31d14156e868259d49eed4
nikochetov <nikochetov@gmail.com>
2022-09-07 21:07:23 +0300

    create data request

--- frontend/src/components/TempLanding.jsx
+++ frontend/src/components/TempLanding.jsx
@@ -1,3 +1,29 @@
-const TempLanding = () => <div>PrivatePage</div>;
+import { useEffect } from 'react';
+import axios from 'axios';
+import routes from '../routes';
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
+
+const TempLanding = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(routes.dataPath(), {
+        headers: getAuthHeader(),
+      });
+      console.log(response.data)
+    };
+
+    fetchData();
+  }, []);
+  return <div>PrivatePage</div>;
+};
 
   export default TempLanding;
