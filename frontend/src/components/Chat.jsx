7dcc517aa5df38752f819c07b16c1028655155e3
nikochetov <nikochetov@gmail.com>
2022-09-20 22:43:53 +0300

    added message input

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, InputGroup, Form } from 'react-bootstrap';
 import { io } from 'socket.io-client';
 import { fetchData } from '../thunks/data-thunk';
 import Messages from './messages/Messages';
@@ -8,29 +8,40 @@ import Channels from './channels/Channels';
 import { UserContext } from '../contexts';
 
 const socket = io();
-socket.on('newMessage', (ev) => {
-  console.log('subscription on new messages:::', ev);
-});
 
 const Chat = () => {
-  // const [formValue, setFormValue] = useState('');
+  const [formValue, setFormValue] = useState('');
   const dispatch = useDispatch();
   const user = useContext(UserContext);
+  const messageInput = useRef(null);
   console.log(user.username);
 
   useEffect(() => {
+    messageInput.current.focus();
     dispatch(fetchData());
   }, [dispatch]);
 
   const clickButton = () => {
-    socket.emit('newMessage', { body: 'hello, world' });
+    socket.emit('newMessage', { body: formValue, username: user.username });
   };
 
   return (
     <div className="mt-3">
       <Channels />
       <Messages />
-      <Button className="mt-3" onClick={() => clickButton()}>Emit message</Button>
+      <InputGroup className="mb-3">
+        <Form.Control
+          ref={messageInput}
+          placeholder="Type message"
+          aria-label="Type message"
+          aria-describedby="basic-addon2"
+          value={formValue}
+          onChange={(ev) => setFormValue(ev.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={() => clickButton()}>
+          Send
+        </Button>
+      </InputGroup>
     </div>
   );
 };
