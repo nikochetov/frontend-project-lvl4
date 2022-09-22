b2541f56671c383b3691410d5b0381698f8104c9
nikochetov <nikochetov@gmail.com>
2022-09-22 23:37:19 +0300

    create message input as separate component

---
+++ frontend/src/components/MessageInput.jsx
@@ -0,0 +1,38 @@
+import { Button, Form, InputGroup } from 'react-bootstrap';
+import { useRef, useEffect, useState } from 'react';
+
+export const MessageInput = ({ onFormSubmit }) => {
+  const [formValue, setFormValue] = useState('');
+  const messageInput = useRef(null);
+
+  useEffect(() => {
+    messageInput.current.focus();
+  }, []);
+
+  const submitForm = () => {
+    onFormSubmit(formValue);
+    setFormValue('');
+    messageInput.current.focus();
+  };
+
+  return (
+    <InputGroup className="mb-3">
+      <Form.Control
+        ref={messageInput}
+        placeholder="Type message"
+        aria-label="Type message"
+        aria-describedby="basic-addon2"
+        value={formValue}
+        onChange={(ev) => setFormValue(ev.target.value)}
+      />
+      <Button
+        type="submit"
+        variant="outline-secondary"
+        id="button-addon2"
+        onClick={() => submitForm()}
+      >
+        Send
+      </Button>
+    </InputGroup>
+  );
+};
