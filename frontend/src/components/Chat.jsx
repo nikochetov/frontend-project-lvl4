ce797ebdda951be9878fadecbf40e72c802952e1
nikochetov <nikochetov@gmail.com>
2023-01-03 22:41:11 +0300

    update chat template

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,13 +1,15 @@
 import { useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import {
+  Container, Row, Col, Button,
+} from 'react-bootstrap';
 import { io } from 'socket.io-client';
 import { fetchData } from '../thunks/data-thunk';
 import Messages from './messages/Messages';
 import Channels from './channels/Channels';
 import { UserContext } from '../contexts';
 import MessageInput from './MessageInput';
-import addChannelIcon from '../assets/icons/plus.svg'
+import addChannelIcon from '../assets/icons/plus.svg';
 
 const socket = io();
 socket.on('newMessage', (ev) => {
@@ -28,7 +30,7 @@ const Chat = () => {
   };
 
   return (
-    <Container md={10} xxl={6} className='h-100 my-4 overflow-hidden rounded shadow'>
+    <Container md={10} xxl={6} className='my-4 rounded shadow'>
       <Row className='h-100 bg-white flex-md-row'>
         <Col xs={3} xl={3} className='border-end px-0 bg-light'>
           <div className='bg-light mb-4 p-3 small d-flex justify-content-between align-items-center'><div><span>Каналы</span></div>
@@ -38,9 +40,9 @@ const Chat = () => {
           </div>
           <Channels />
         </Col>
-        <Col xs={9} xxl={6} className='col p-0 h-100 '>
+        <Col xs={9} xxl={6} className='p-0'>
           <div className='bg-light mb-4 p-3 shadow-sm small'><span>Сообщения</span></div>
-          <div className='chat-messages overflow-auto px-5 '>
+          <div className='chat-messages overflow-hidden px-5'>
           <Messages><MessageInput onFormSubmit={clickButton}/></Messages>
           </div>
         </Col>
