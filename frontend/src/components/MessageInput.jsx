462f81c8a2e56f9b18052098ad4dc594bada899e
nikochetov <nikochetov@gmail.com>
2023-04-22 21:18:21 +0300

    update message submit handle

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,40 +1,52 @@
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import React from 'react';
+import { useFormik } from 'formik';
 import sendImage from '../assets/icons/send.svg';
 
 const MessageInput = ({ onFormSubmit }) => {
-  const [formValue, setFormValue] = React.useState('');
   const messageInput = React.useRef(null);
+
+  const formik = useFormik({
+    initialValues: {
+      message: '',
+    },
+    onSubmit: (values) => {
+      onFormSubmit(values.message);
+    },
+  });
 
   React.useEffect(() => {
     messageInput.current.focus();
   }, []);
 
   const submitForm = () => {
-    onFormSubmit(formValue);
-    setFormValue('');
     messageInput.current.focus();
   };
 
   return (
-    <InputGroup className="mt-3 mb-3">
-      <Form.Control
-        ref={messageInput}
-        placeholder="Введите сообщение..."
-        aria-label="Введите сообщение..."
-        aria-describedby="basic-addon2"
-        value={formValue}
-        onChange={(ev) => setFormValue(ev.target.value)}
-      />
-      <Button
-        type="submit"
-        variant="outline-secondary"
-        id="button-addon2"
-        onClick={() => submitForm()}
-      >
-        <img src={sendImage} alt="Send message" placeholder="Отправить сообщение" />
-      </Button>
-    </InputGroup>
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group controlId="message">
+        <InputGroup className="mt-3 mb-3">
+          <Form.Control
+            ref={messageInput}
+            placeholder="Введите сообщение..."
+            aria-label="Введите сообщение..."
+            aria-describedby="basic-addon2"
+            name="message"
+            onChange={formik.handleChange}
+            value={formik.values.message}
+          />
+          <Button
+            type="submit"
+            variant="outline-secondary"
+            id="button-addon2"
+            onClick={() => submitForm()}
+          >
+            <img src={sendImage} alt="Send message" placeholder="Отправить сообщение" />
+          </Button>
+        </InputGroup>
+      </Form.Group>
+    </Form>
   );
 };
 
