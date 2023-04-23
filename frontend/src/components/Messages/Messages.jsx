d1908602742205b5789c43f58f3d9862a4381c88
nikochetov <nikochetov@gmail.com>
2023-04-23 22:12:15 +0300

    autoscroll

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,9 +1,20 @@
 import { useSelector } from 'react-redux';
 import { ListGroup } from 'react-bootstrap';
+import React from 'react';
 import Message from './Message';
 
 const Messages = ({ children }) => {
   const messages = useSelector((state) => state.messagesState.channelMessages);
+
+  const messagesEndRef = React.useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'auto' });
+  };
+
+  React.useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return messages && (
     <>
@@ -11,6 +22,7 @@ const Messages = ({ children }) => {
         {messages.map(({ id, body, username }) => (
           <Message key={id} body={body} author={username} />
         ))}
+        <div ref={messagesEndRef} />
       </ListGroup>
       {children}
     </>
