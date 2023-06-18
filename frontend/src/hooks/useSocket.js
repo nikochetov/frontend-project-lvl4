c6e23146cf8771124e174322d831364d039cace7
nikochetov <nikochetov@gmail.com>
2023-06-19 00:14:07 +0300

    socket disconnect on logout

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,9 +1,18 @@
 import { useDispatch } from 'react-redux';
+import React from 'react';
 import socketRequestKind from '../constants/socket-request-kind';
 import { actions as messagesActions } from '../slices/messagesSlice';
 import { actions as channelsActions } from '../slices/channelsSlice';
+import SocketConnectionCondition from '../constants/socket-connection-condition';
 
 const useSocket = (socket) => {
+  // const [socketConnectionCondition, setSocketConnectionCondition] = React.useState('idle');
+  // const [socketAction, setSocketAction] = React.useState(null);
+
+  React.useEffect(() => () => {
+    socket.disconnect();
+  }, []);
+
   const dispatch = useDispatch();
   socket.on(socketRequestKind.newMessage, (ev) => {
     dispatch(messagesActions.addMessage(ev));
@@ -19,6 +28,20 @@ const useSocket = (socket) => {
     dispatch(channelsActions.removeChannel(ev));
     dispatch(channelsActions.setCurrentChannelId(1));
   });
+
+  // React.useEffect(() => {
+  //   notify(socketConnectionCondition);
+  // }, [socketConnectionCondition]);
+
+  // socket.on(SocketConnectionCondition.connect, () => {
+  //   setSocketConnectionCondition(socketConnectionCondition === 'error' ? 'connected' : socketConnectionCondition);
+  // });
+  // socket.on(SocketConnectionCondition.disconnect, () => {
+  //   setSocketConnectionCondition('disconnected');
+  // });
+  // socket.on(SocketConnectionCondition.error, () => {
+  //   setSocketConnectionCondition('error');
+  // });
 
   return socket;
 };
